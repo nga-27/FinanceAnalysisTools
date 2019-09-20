7c32aa5217683348f6e211c1a8a065bf4e665c12
nga-27 <namell91@gmail.com>
2019-09-19 23:15:22 -0500

    remove pycache

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,1 +1,2 @@
 from .file_importer import get_finance_file_data 
+from .startup import startup_script
---
+++ libs/startup.py
@@ -0,0 +1,33 @@
+import pandas as pd 
+import numpy as np 
+import json
+import os
+import time
+
+def startup_script(version='0.0.1', update_release='2019-09-19') -> dict:
+    print(" ")
+    print("----------------------------------")
+    print("-   Financial Analysis Tools     -")
+    print("-                                -")
+    print("-            nga-27              -")
+    print("-                                -")
+    print(f"-       version: {version}          -")
+    print(f"-       updated: {update_release}      -")
+    print("----------------------------------")
+    print(" ")
+    
+    time.sleep(1)
+
+    return get_configuration()
+
+
+def get_configuration() -> dict:
+    json_path = 'config.json'
+    if os.path.exists(json_path):
+        with open(json_path) as json_file:
+            core = json.load(json_file)
+            return core
+
+    print(f"No 'config.json' found. Exiting...")
+    return None
+        
